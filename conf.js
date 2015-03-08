6f2300d132a6e52fc62c8b89f75df08ec939df99
anthonywittemann <awittema@usc.edu>
2015-03-08 16:36:17 -0700

    test 1 working

---
+++ conf.js
@@ -0,0 +1,4 @@
+exports.config = {
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['functional-req-1.js', 'functional-req-2.js', 'functional-req-3.js', 'functional-req-4.js', 'functional-req-2.js','functional-req-6.js', 'functional-req-7.js',]
+};
